c5b4f280388ac782116bb0cfa6f20808b249ce13
ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2026-06-23 17:33:47 +0500

    my first python online class

---
+++ app.py
@@ -0,0 +1,73 @@
+# print("hello")
+
+# install python
+# python or python3 --version
+
+# functions
+# builten formula - uncontrolled 
+# (Date - months(specific))
+# pre defined code which is reusable
+# controlled code
+# advantage - reuse , maintain , controlled code ,readable
+
+# def my_function():
+#     print("hello function")
+
+# my_function()
+# my_function()
+# my_function()
+
+# def myFirstFunction():  #camelcase (drawmadory)
+#  a = 4
+# def calculate():
+#     sum = a + 5
+#     print(sum)
+
+# calculate()
+
+#  a = 6
+# def calculate():
+#     sum = a + 5
+#     print(sum)
+# calculate()
+
+# def calculation(sum): # sum -parameter
+#  return sum + 5
+
+# print(calculation(7)) # 7 - argument
+
+# def my_name(name):
+#     print(name)
+
+# my_name("abc") 
+
+# def sum(number1,number2=0): #default parameter
+#     print(number1 + number2)
+
+# # sum(2,3)  
+# sum(2)
+ 
+# def sum(a,b):
+#  print(a + b)
+# sum(2,3)    
+
+# print(sum())
+
+# def message():
+#   return ("hello message")
+# newmessage = message()  
+
+# print(newmessage)
+
+def multiply(a,b):
+    print( a * b)
+
+multiply(3,4)    
+
+
+def myfullname(firstname,lastname):
+    print("my first name is" firstname, " my " lastname)
+
+myfullname("ibrahim", "khan")
+
+# git - software
