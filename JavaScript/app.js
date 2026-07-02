eb67a7434e0850f8b21abe17004007bca4d07279
ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2026-07-02 17:27:49 +0500

    js initialized

---
+++ JavaScript/app.js
@@ -0,0 +1,16 @@
+// install node js 
+// js for terminal(backend)
+// for run use (node app.js)
+// console.log("javascript is running")
+
+// js for browser
+// create index.html <- (.html is must)
+// HTML(hyper text markup language)
+// now link js in html with the help of script tag (<script>)
+
+// now broser js start here
+console.log("js in html file")
+console.log("js in html file 2")
+// alert("hello alert")
+// prompt("enter your name")
+// click f12 or ctr+shitf+i in html browser to see js
